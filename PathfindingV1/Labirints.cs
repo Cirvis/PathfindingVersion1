f6b8179bd6add54ef6cd309c1fe5aac79d27d1f7
Edgars Čiževskis <Edgars.Cizevskis@edu.rtu.lv>
2019-09-30 18:46:58 +0300

    Working Basic Alpha

---
+++ PathfindingV1/Labirints.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Labirints : IPathNode<Object>
+{
+    public Int32 Platums { get; set; }
+    public Int32 Augstums { get; set; }
+    public Boolean Siena { get; set; }
+
+    public bool NavSiena(Object unused)
+    {
+        return !Siena;
+    }
+}
---
+++ PathfindingV1/PathfindingV1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace PathfindingV1
+{
+    class Program
+    {
+        public static int platums = 4;
+        public static int augstums = 6;
+        static void Main(string[] args)
+        {
+            Labirints[,] grid = new Labirints[platums, augstums];
+
+            for (int x = 0; x < platums; x++)
+            {
+                for (int y = 0; y < augstums; y++)
+                {
+                    Boolean siena = false;  // aizstāt ar faila sienu koordinātem
+                    if (siena)
+                    {
+                        Console.WriteLine(x + " " +y);
+                    }
+                    grid[x, y] = new Labirints()
+                    {
+                        Siena = siena,
+                        Platums = x,
+                        Augstums = y,
+                    };
+                }
+            }
+            MySolver<Labirints, Object> aStar = new MySolver<Labirints, Object>(grid);
+            LinkedList<Labirints> path = aStar.Search(new Point(2, 2), new Point(platums-1, augstums-1), null);
+            aStar.Search(new Point(2, 2), new Point(platums-1, augstums-1), null);
+            
+            if (path != null)
+            {
+                foreach (Labirints node in path)
+                    Console.Write("(" + node.Platums + " " + node.Augstums + ")");
+
+                Console.WriteLine("\n");
+            }
+            
+        }
+    }
+    public class MySolver<TPathNode, TUserContext> : PathfindingV1.SpatialAStar<TPathNode, TUserContext> where TPathNode : PathfindingV1.IPathNode<TUserContext>
+    {
+        protected override Double Heuristic(PathNode inStart, PathNode inEnd)
+        {
+            return Math.Abs(inStart.X - inEnd.X) + Math.Abs(inStart.Y - inEnd.Y);
+        }
+
+        protected override Double NeighborDistance(PathNode inStart, PathNode inEnd)
+        {
+            return Heuristic(inStart, inEnd);
+        }
+
+        public MySolver(TPathNode[,] inGrid)
+            : base(inGrid)
+        {
+        }
+    }
+}
